7745a3ca7ffc5b053300b39f827f253a022822f5
Damir Khaibuloev <damir4ik2004@gmail.com>
2025-12-11 21:34:23 +0500

    Initial commit

---
+++ fft_plot.py
@@ -0,0 +1,44 @@
+import os
+import matplotlib.pyplot as plt
+import numpy as np
+
+plt.style.use("seaborn-v0_8-darkgrid")  # красивый стиль
+
+base = os.path.dirname(os.path.abspath(__file__))
+
+files = [
+    "fft_graphics_10.dat",
+    "fft_graphics_50.dat",
+    "fft_graphics_100.dat",
+    "fft_graphics_1000.dat",
+    "fft_graphics_10000.dat"
+]
+
+for fname in files:
+    path = os.path.join(base, fname)
+
+    data = np.loadtxt(path, skiprows=1)
+
+    X = data[:, 0]
+    Y = data[:, 1]
+    D = data[:, 2]
+
+    plt.figure(figsize=(12, 7))
+
+    # --- Графики ---
+    plt.plot(X, Y, label="Analytic", linewidth=2.5, color="#FF9500")
+    plt.scatter(X, Y, s=12, color="#FF9500")
+
+    plt.plot(X, D, label="DFT", linewidth=2.5, color="#0066FF")
+    plt.scatter(X, D, s=12, color="#0066FF")
+
+    # --- Оформление ---
+    plt.title(f"Сравнение аналитического решения и DFT\n({fname})", fontsize=16)
+    plt.xlabel("X", fontsize=14)
+    plt.ylabel("Y", fontsize=14)
+    plt.grid(True, alpha=0.3)
+
+    plt.legend(framealpha=0.9, fontsize=12)
+    plt.tight_layout()
+
+    plt.show()
---
+++ spline.gnu
@@ -0,0 +1,23 @@
+reset
+
+set terminal wxt enhanced size 1100,700
+set style line 1 lc rgb "orange" lw 2
+set style line 2 lc rgb "blue"   lw 2
+
+set grid xtics ytics mxtics mytics
+set xlabel "x"
+set ylabel "f(x)"
+
+Ns = "10 50 100 1000 10000"
+
+do for [n in Ns] {
+    filename = sprintf("spline_graphics_%s.dat", n)
+    title = sprintf("Сравнение DFT для N = %s", n)
+
+    set title title
+
+    plot filename using 1:2 with lines ls 1 title "analytic", \
+         filename using 1:3 with lines ls 2 title "DFT"
+
+    pause -1   # ← ДЕЛАЕТ ОКНО ИНТЕРАКТИВНЫМ И ЗУМИРУЕМЫМ
+}
